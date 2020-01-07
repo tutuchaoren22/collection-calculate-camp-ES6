7bd9394fea1ecd9cf669663a54aeaf7955223bda
duyuqian <15738381799@163.com>
2020-01-07 17:29:00 +0800

    [duyuqian] refactor:  refactor practice_1 in section_3

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,8 +1,8 @@
 function create_updated_collection(collection_a, object_b) {
-    var itemToDecrease = object_b.value;
-    for (var i in itemToDecrease) {
-        var item = itemToDecrease[i];
-        for (var j = 0; j < collection_a.length; j++) {
+    let itemToDecrease = object_b.value;
+    for (let i in itemToDecrease) {
+        let item = itemToDecrease[i];
+        for (let j = 0; j < collection_a.length; j++) {
             if (item === collection_a[j].key) {
                 collection_a[j].count -= 1;
                 break;
--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,10 +1,10 @@
 function create_updated_collection(collection_a, object_b) {
-    var itemToDecrease = object_b.value;
+    let itemToDecrease = object_b.value;
     for (var i in itemToDecrease) {
-        var item = itemToDecrease[i];
-        for (var j = 0; j < collection_a.length; j++) {
+        let item = itemToDecrease[i];
+        for (let j = 0; j < collection_a.length; j++) {
             if (item === collection_a[j].key) {
-                var numberToDecrease = parseInt(collection_a[j].count / 3);
+                let numberToDecrease = parseInt(collection_a[j].count / 3);
                 collection_a[j].count -= numberToDecrease;
                 break;
             }
