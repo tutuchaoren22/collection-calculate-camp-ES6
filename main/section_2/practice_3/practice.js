880494d44f88b76a06e9c6db1a6613120d9aeb03
duyuqian <15738381799@163.com>
2020-01-07 10:20:57 +0800

    [duyuqian] refactor:  refactor practice_3 in section_2

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,9 +1,11 @@
 function count_same_elements(collection) {
-    var countSameElements = [];
-    for (var i = 0; i < collection.length; i++) {
-        var sameElements = {};
-        var item = collection[i];
-        var lengthOfElements = countSameElements.length;
+    let countSameElements = [];
+    let i;
+    const len = collection.length;
+    for (i = 0; i < len; i++) {
+        let sameElements = {};
+        const item = collection[i];
+        const lengthOfElements = countSameElements.length;
         if (lengthOfElements) {
             addToCount(item, lengthOfElements, sameElements, countSameElements);
         } else {
@@ -15,7 +17,7 @@ function count_same_elements(collection) {
 
 
 function addToCount(item, lengthOfElements, sameElements, countSameElements) {
-    var itemCount = item.split(/-|\[|\]|:/);
+    let itemCount = item.split(/-|\[|\]|:/);
     item = itemCount[0];
     itemCount = Number(itemCount[1]);
     if (countSameElements[lengthOfElements - 1].name === item) {
