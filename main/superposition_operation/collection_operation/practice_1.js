cc8c11712cb65e342aace2bd377143800691e441
duyuqian <15738381799@163.com>
2020-01-07 17:33:00 +0800

    [duyuqian] refactor:  refactor practice1&2&3 in collection operation

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function hybrid_operation(collection) {
-    var afterMultiplyThreeAddTwo = multiplyThreeAddTwo(collection);
-    var sum = sumOperation(afterMultiplyThreeAddTwo);
+    let afterMultiplyThreeAddTwo = multiplyThreeAddTwo(collection);
+    let sum = sumOperation(afterMultiplyThreeAddTwo);
     return sum;
 }
 
 function multiplyThreeAddTwo(collection) {
-    var afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 2));
+    let afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 2));
     return afterMultiplyThreeAddTwo;
 }
 
 function sumOperation(collection) {
-    var sum = collection.reduce(function(x, y) {
+    let sum = collection.reduce(function(x, y) {
         return x + y;
     });
     return sum;
--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,24 +1,24 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-    var oddNumber = findOddNumber(collection);
-    var afterMultiplyThreeAddTwo = multiplyThreeAddTwo(oddNumber);
-    var sum = sumOperation(afterMultiplyThreeAddTwo);
+    let oddNumber = findOddNumber(collection);
+    let afterMultiplyThreeAddTwo = multiplyThreeAddTwo(oddNumber);
+    let sum = sumOperation(afterMultiplyThreeAddTwo);
     return sum;
 }
 
 function findOddNumber(collection) {
-    var oddNumber = collection.filter(x => (x % 2 !== 0));
+    let oddNumber = collection.filter(x => (x % 2 !== 0));
     return oddNumber;
 }
 
 function multiplyThreeAddTwo(collection) {
-    var afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 5));
+    let afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 5));
     return afterMultiplyThreeAddTwo;
 }
 
 function sumOperation(collection) {
-    var sum = collection.reduce(function(x, y) {
+    let sum = collection.reduce(function(x, y) {
         return x + y;
     });
     return sum;
